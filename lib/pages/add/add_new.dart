bc552d679dd4509dc217247d3540010f2bd20d89
olaomar2000 <olashab300@gmail.com>
2021-08-02 12:23:09 +0300

    data

--- lib/pages/add/add_new.dart
+++ lib/pages/add/add_new.dart
@@ -1,9 +1,66 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:project2_gsg/database/repository.dart';
+import 'package:project2_gsg/models/m_type.dart';
+import 'package:project2_gsg/models/pill.dart';
 import 'package:project2_gsg/pages/add/typecard.dart';
 import 'package:project2_gsg/platform_flat_button.dart';
+import 'package:project2_gsg/snack_bar.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+class add_new extends StatefulWidget {
+  @override
+  State<add_new> createState() => _add_newState();
+}
 
-class add_new extends StatelessWidget {
+class _add_newState extends State<add_new> {
+
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final Snackbar snackbar = Snackbar();
+  //medicine types
+  final List<String> weightValues = ["pills", "ml", "mg"];
+
+  //list of medicines forms objects
+  final List<MedicineType> medicineTypes = [
+    MedicineType("Syrup", Image.asset("assets/images/syrup.png"), true),
+    MedicineType(
+        "Pill", Image.asset("assets/images/pills.png"), false),
+    MedicineType(
+        "Capsule", Image.asset("assets/images/capsule.png"), false),
+    MedicineType(
+        "Cream", Image.asset("assets/images/cream.png"), false),
+    MedicineType(
+        "Drops", Image.asset("assets/images/drops.png"), false),
+    MedicineType(
+        "Syringe", Image.asset("assets/images/syringe.png"), false),
+  ];
+
+  //-------------Pill object------------------
+  int howManyWeeks = 1;
+  String selectWeight;
+  DateTime setDate = DateTime.now();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+
+  //==========================================
+
+  //-------------- Database and notifications ------------------
+  final Repository _repository = Repository();
+
+
+  //============================================================
+
+  @override
+  void initState() {
+    super.initState();
+    selectWeight = weightValues[0];
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -51,14 +108,16 @@ class add_new extends StatelessWidget {
                             fontSize: 18,
                             fontWeight: FontWeight.bold)),
                     SizedBox(height: 10),
-                    Row(
-                      children: [
-                         MedicineTypeCard(),
-                         MedicineTypeCard(),
-                         MedicineTypeCard(),
-                         MedicineTypeCard(),
-
-                      ],
+                    Container(
+                      height: 100,
+                      child: ListView(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          ...medicineTypes.map(
+                                  (type) => MedicineTypeCard(type, medicineTypeClick))
+                        ],
+                      ),
                     ),
                     SizedBox(height: 10),
                     Text('Time & Shedule',
@@ -121,4 +180,133 @@ class add_new extends StatelessWidget {
       ),
     );
   }
+
+
+
+  void sliderChanged(double value) =>
+      setState(() => this.howManyWeeks = value.round());
+
+  //choose popum menu item
+  void popUpMenuItemChanged(String value) =>
+      setState(() => this.selectWeight = value);
+
+  //------------------------OPEN TIME PICKER (SHOW)----------------------------
+  //------------------------CHANGE CHOOSE PILL TIME----------------------------
+
+  Future<void> openTimePicker() async {
+    await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+        helpText: "Choose Time")
+        .then((value) {
+      DateTime newDate = DateTime(
+          setDate.year,
+          setDate.month,
+          setDate.day,
+          value != null ? value.hour : setDate.hour,
+          value != null ? value.minute : setDate.minute);
+      setState(() => setDate = newDate);
+      print(newDate.hour);
+      print(newDate.minute);
+    });
+  }
+
+  //====================================================================
+
+  //-------------------------SHOW DATE PICKER AND CHANGE CURRENT CHOOSE DATE-------------------------------
+  Future<void> openDatePicker() async {
+    await showDatePicker(
+        context: context,
+        initialDate: setDate,
+        firstDate: DateTime.now(),
+        lastDate: DateTime.now().add(Duration(days: 100000)))
+        .then((value) {
+      DateTime newDate = DateTime(
+          value != null ? value.year : setDate.year,
+          value != null ? value.month : setDate.month,
+          value != null ? value.day : setDate.day,
+          setDate.hour,
+          setDate.minute);
+      setState(() => setDate = newDate);
+      print(setDate.day);
+      print(setDate.month);
+      print(setDate.year);
+    });
+  }
+
+  //=======================================================================================================
+
+  //--------------------------------------SAVE PILL IN DATABASE---------------------------------------
+  Future savePill() async {
+    //check if medicine time is lower than actual time
+    if (setDate.millisecondsSinceEpoch <=
+        DateTime.now().millisecondsSinceEpoch) {
+      snackbar.showSnack(
+          "Check your medicine time and date", _scaffoldKey, null);
+    } else {
+      //create pill object
+      Pill pill = Pill(
+          amount: amountController.text,
+          howManyWeeks: howManyWeeks,
+          medicineForm: medicineTypes[medicineTypes.indexWhere((element) => element.isChoose == true)].name,
+          name: nameController.text,
+          time: setDate.millisecondsSinceEpoch,
+          type: selectWeight,
+         );
+
+      //---------------------| Save as many medicines as many user checks |----------------------
+      for (int i = 0; i < howManyWeeks; i++) {
+        dynamic result =
+        await _repository.insertData("Pills", pill.pillToMap());
+        if (result == null) {
+          snackbar.showSnack("Something went wrong", _scaffoldKey, null);
+          return;
+        } else {
+          //set the notification schneudele
+          tz.initializeTimeZones();
+          tz.setLocalLocation(tz.getLocation('Europe/Warsaw'));
+      //    await _notifications.showNotification(pill.name, pill.amount + " " + pill.medicineForm + " " + pill.type, time);
+          setDate = setDate.add(Duration(milliseconds: 604800000));
+          pill.time = setDate.millisecondsSinceEpoch;
+
+        }
+      }
+      //---------------------------------------------------------------------------------------
+      snackbar.showSnack("Saved", _scaffoldKey, null);
+      Navigator.pop(context);
+    }
+  }
+
+  //=================================================================================================
+
+  //----------------------------CLICK ON MEDICINE FORM CONTAINER----------------------------------------
+  void medicineTypeClick(MedicineType medicine) {
+    setState(() {
+      medicineTypes.forEach((medicineType) => medicineType.isChoose = false);
+      medicineTypes[medicineTypes.indexOf(medicine)].isChoose = true;
+    });
+  }
+
+  //=====================================================================================================
+
+  //get time difference
+  int get time =>
+      setDate.millisecondsSinceEpoch -
+          tz.TZDateTime.now(tz.local).millisecondsSinceEpoch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
--- lib/pages/add/typecard.dart
+++ lib/pages/add/typecard.dart
@@ -1,32 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:project2_gsg/models/m_type.dart';
 
 
+
+// class MedicineTypeCard extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+//         Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Container(
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(10.0),
+//               color: Color(0xff7379CD),
+//             ),
+//             height: 70,
+//             width: 100,
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 SizedBox(height: 5.0,),
+//                 Container(child: Icon(Icons.local_drink,color: Colors.white,),),
+//                 SizedBox(height: 7.0,),
+//               ],
+//             ),
+//
+//
+//           ),
+//         );
+//
+//   }
+// }
 
 class MedicineTypeCard extends StatelessWidget {
+  final MedicineType pillType;
+  final Function handler;
+  MedicineTypeCard(this.pillType,this.handler);
   @override
   Widget build(BuildContext context) {
-    return
-        Padding(
-          padding: const EdgeInsets.all(8.0),
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: () => handler(pillType),
           child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.0),
-              color: Color(0xff7379CD),
+              color: pillType.isChoose ? Color.fromRGBO(7, 190, 200, 1) :Colors.white,
             ),
-            height: 70,
             width: 100,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(height: 5.0,),
-                Container(child: Icon(Icons.local_drink,color: Colors.white,),),
+                Container(width:50,height: 50.0,child: pillType.image),
                 SizedBox(height: 7.0,),
+                Container(child: Text(pillType.name,style: TextStyle(
+                    color:pillType.isChoose ? Colors.white : Colors.black,fontWeight: FontWeight.w500
+                ),)),
               ],
             ),
 
-
           ),
-        );
-
+        ),
+        SizedBox(width: 15.0,)
+      ],
+    );
   }
 }
